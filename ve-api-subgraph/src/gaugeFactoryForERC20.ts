9ccbbfa17db58dd5a31ca12087a90a2ba3f78167
diego <pythinking@gmail.com>
2023-11-15 16:43:28 -0300

    GaugeFactoryForERC20 contract

---
+++ ve-api-subgraph/src/gaugeFactoryForERC20.ts
@@ -0,0 +1,36 @@
+import { BigInt, Address } from '@graphprotocol/graph-ts'
+import {
+  GaugeFactoryForERC20,
+  GaugeCreated as GaugeCreatedEvent,
+  ImplementationChanged as ImplementationChangedEvent,
+  Initialized as InitializedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent
+} from '../generated/GaugeFactoryForERC20/GaugeFactoryForERC20'
+import { GaugeCreated, ImplementationChanged, Initialized, OwnershipTransferred } from '../generated/schema'
+
+export function handleGaugeCreated(event: GaugeCreatedEvent): void {
+  let entity = new GaugeCreated(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+  entity.gauge = event.params.gauge.toHexString()
+  entity.pool = event.params.pool.toHexString()
+  entity.save()
+}
+
+export function handleImplementationChanged(event: ImplementationChangedEvent): void {
+  let entity = new ImplementationChanged(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+  entity.oldImplementation = event.params.oldImplementation.toHexString()
+  entity.newImplementation = event.params.newImplementation.toHexString()
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+  entity.version = event.params.version
+  entity.save()
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
+  let entity = new OwnershipTransferred(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+  entity.previousOwner = event.params.previousOwner.toHexString()
+  entity.newOwner = event.params.newOwner.toHexString()
+  entity.save()
+}
